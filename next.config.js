86f2920ddfe5c2690d46d4532e7248e3d41d086b
Dipraise1 <92402915+Dipraise1@users.noreply.github.com>
2023-02-21 17:49:17 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const { default: build } = require('next/dist/build')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+ 
